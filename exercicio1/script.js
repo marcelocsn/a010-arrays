202ce75b713f7b1e71d2200c05a28c7e1cca8f94
csnicolela.marcelo@gmail.com <csnicolela.marcelo@gmail.com>
2022-09-01 15:35:44 -0300

    Exercicios Aula 10 dia 01/09

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const primeiroArray = [13, 8, 25, 51]
+
+const segundoArray = ["Marcelo", "Vanessa", "Théo"]
+
+const terceiroArray = [25, "Marcelo", true]
+
+console.log(`Essas são as arrays: \n${primeiroArray}\n${segundoArray}\n${terceiroArray}`)
